86cc0502ecd6f7f06c0663e0313f271214fc0fd3
OmarEmarah-CPU <195971593+OmarEmarah-CPU@users.noreply.github.com>
2025-12-10 15:16:15 +0200

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "Zero",
+    'version': '1.0',
+    'depends': ['base','sale_management', 'stock','website_sale','product'],
+    'author': "Omar",
+    'category': 'Category',
+    'description': """
+    Restricts Quantity to Zero
+    """,
+    'data': [
+        'views/Zero_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
